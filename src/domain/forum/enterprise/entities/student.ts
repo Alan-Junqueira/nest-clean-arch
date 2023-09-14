dbd78b36a43c3361512e00414a9e0bbc41c0fd94
alan-junqueira <contato.alanjunqueira@gmail.com>
2023-09-14 18:20:36 -0300

    feat: Create auth and register use cases; create encrypt and has abstraction.

--- src/domain/forum/enterprise/entities/student.ts
+++ src/domain/forum/enterprise/entities/student.ts
@@ -3,9 +3,23 @@ import { UniqueEntityId } from '@/core/entity/unique-entity-id'
 
 interface IStudentProps {
   name: string
+  email: string
+  password: string
 }
 
 export class Student extends Entity<IStudentProps> {
+  get name() {
+    return this.props.name
+  }
+
+  get email() {
+    return this.props.email
+  }
+
+  get password() {
+    return this.props.password
+  }
+
   static create(props: IStudentProps, id?: UniqueEntityId) {
     const student = new Student(
       {
