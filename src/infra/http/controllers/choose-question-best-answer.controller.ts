2e5422941304fb7a1b5a88c1348c20197d884e1d
alan-junqueira <contato.alanjunqueira@gmail.com>
2023-09-19 22:53:19 -0300

    feat: Implement funcitonality to choose best answer.

---
+++ src/infra/http/controllers/choose-question-best-answer.controller.ts
@@ -0,0 +1,36 @@
+import { ChooseQuestionBestAnswerUseCase } from '@/domain/forum/application/use-cases/choose-question-best-answer'
+import { CurrentUser } from '@/infra/auth/current-user.decorator'
+import { TUserPayload } from '@/infra/auth/jwt.strategy'
+import {
+  BadRequestException,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+} from '@nestjs/common'
+
+@Controller('/answers/:answerId/choose-as-best')
+export class ChooseQuestionBestAnswerController {
+  constructor(
+    private chooseQuestionBestAnswer: ChooseQuestionBestAnswerUseCase,
+  ) {}
+
+  @Patch()
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async handle(
+    @CurrentUser() user: TUserPayload,
+    @Param('answerId') answerId: string,
+  ) {
+    const { sub: userId } = user
+
+    const result = await this.chooseQuestionBestAnswer.execute({
+      authorId: userId,
+      answerId,
+    })
+
+    if (result.isLeft()) {
+      throw new BadRequestException()
+    }
+  }
+}
