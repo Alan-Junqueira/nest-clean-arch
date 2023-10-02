0039a6666f3f1597fc4211d36fdb5372f15bebc3
alan-junqueira <contato.alanjunqueira@gmail.com>
2023-10-01 23:02:42 -0300

    feat: add author when returning a comment

--- src/infra/http/controllers/fetch-question-comments.controller.ts
+++ src/infra/http/controllers/fetch-question-comments.controller.ts
@@ -8,7 +8,7 @@ import {
 import { z } from 'zod'
 import { ZodValidationPipe } from '../pipes/zod-validation.pipe'
 import { FetchQuestionCommentsUseCase } from '@/domain/forum/application/use-cases/fetch-question-comments'
-import { CommentPresenter } from '../presenters/comment.presenter'
+import { CommentWithAuthorPresenter } from '../presenters/comment-with-author-presenter'
 
 const pageQueryParamSchema = z
   .string()
@@ -36,8 +36,8 @@ export class FetchQuestionCommentsController {
       throw new BadRequestException()
     }
 
-    const { questionComments } = result.value
+    const { comments } = result.value
 
-    return { questionComments: questionComments.map(CommentPresenter.toHTTP) }
+    return { questionComments: comments.map(CommentWithAuthorPresenter.toHTTP) }
   }
 }
